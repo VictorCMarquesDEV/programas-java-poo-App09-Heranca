42ca316a8a0f3154da07eea4fcd889651e07dd09
Victor Cavalcanti <victorcmarques.dev@gmail.com>
2024-04-13 13:00:24 -0300

    Add App09

---
+++ App09.Heranca/App09/App09/Aluno.java
@@ -0,0 +1,27 @@
+package App09;
+
+public class Aluno extends Pessoa{
+	private int matr;
+	private String curso;
+
+
+	public int getMatr() {
+		return matr;
+	}
+	public void setMatr(int matr) {
+		this.matr = matr;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	
+	
+	public void cancelarMatr() {
+		this.setCurso(null);
+		this.setMatr(0);
+		System.out.println("Matrícula cancelada");
+	}
+}
---
+++ App09.Heranca/App09/App09/Master.java
@@ -0,0 +1,28 @@
+package App09;
+
+public class Master {
+
+	public static void main(String[] args) {
+		Pessoa p1 = new Pessoa();
+		Aluno p2 = new Aluno();
+		Professor p3 = new Professor();
+		Funcionario p4 = new Funcionario();
+		
+		p1.setNome("Pedro");
+		p2.setNome("Maria");
+		p3.setNome("Cláudio");
+		p4.setNome("Fabiana");
+		
+		p1.setSexo("M");
+		p2.setIdade(18);
+		p2.setCurso("Informática");
+		p3.setSalario(2500.75f);
+		p4.setSexo("F");
+		p4.setSetor("Estoque");
+		
+		p1.detalhar();
+		p2.detalhar();
+		p3.detalhar();
+		p4.detalhar();
+	}
+}
